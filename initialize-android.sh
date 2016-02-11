a3b9a0f360ee18cf9eb333b96a6fdb6c398a68e1
Mauricio Faustino <mcastro@thoughtworks.com>
2016-02-11 10:42:43 -0300

    adding build tools version 23

--- initialize-android.sh
+++ initialize-android.sh
@@ -16,8 +16,9 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
   # The BuildTools version used by your project
   echo y | android update sdk --no-ui --filter build-tools-22.0.1 --all > /dev/null
   echo y | android update sdk --no-ui --filter build-tools-21.1.2 --all > /dev/null
+  echo y | android update sdk --no-ui --filter build-tools-23.0.2 --all > /dev/null
   # The SDK version used to compile your project
-  # echo y | android update sdk --no-ui --filter android-22 > /dev/null
+  echo y | android update sdk --no-ui --filter android-23 > /dev/null
 
   # uncomment to install the Extra/Android Support Library
   echo y | android update sdk --no-ui --filter extra-android-support --all > /dev/null
